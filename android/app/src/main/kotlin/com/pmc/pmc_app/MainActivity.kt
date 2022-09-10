2c25c20221ae23c6c0b3b7a8336d80a6c0192d9b
pmc-muchiri <pmc.ac.ke@gmail.com>
2022-09-10 17:37:02 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/pmc/pmc_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.pmc.pmc_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+//the main function is the start of all our flutter app
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Center(
+        child: Text('Hello world pmc'),
+      ),
+    ),
+  );
+}
